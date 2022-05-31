e4f5f72d6ac3dba2bf692802308bf597950ed4d3
rudy <doomdexter07@gmail.com>
2022-05-31 09:48:13 +0530

    created all test for genre end point

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -2,6 +2,7 @@ const request = require("supertest");
 const mongoose = require("mongoose");
 
 const { Genre } = require("../../model/genre");
+const { User } = require("../../model/user");
 let server;
 
 describe("/pasty/genres", () => {
@@ -29,5 +30,62 @@ describe("/pasty/genres", () => {
       expect(res.status).toBe(200);
       expect(res.body.some((g) => g.title === "Free World")).toBeTruthy();
     });
+  });
+
+  describe("GET/:id", () => {
+    it("should return a valid genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "horrror" });
+      await genre.save();
+
+      const res = await request(server).get("/pasty/genres/" + genre._id);
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty("name", genre.name);
+    });
+
+    it("should return 404 if valid id is not passed", async () => {
+      const res = await request(server).get("/pasty/genres/1");
+      expect(res.status).toBe(404);
+    });
+  });
+
+  describe("POST", () => {
+    it("should return 401 if client is not logged in", async () => {
+      const res = await request(server)
+        .post("/pasty/genres")
+        .send({ name: "Genre1" });
+      expect(res.status).toBe(401);
+    });
+
+    it("should return 400 if genre is less than 3", async () => {
+      const token = new User().generateAuthToken();
+      const res = await request(server)
+        .post("/pasty/genres")
+        .set("x-auth-token", token)
+        .send({ name: "ge" });
+      expect(res.status).toBe(400);
+    });
+  });
+
+  it("should save the genre if everything is perfect", async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post("/pasty/genres")
+      .set("x-auth-token", token)
+      .send({ name: "genre1" });
+    const genre = await Genre.find({ name: "genre1" });
+    expect(res.status).toBe(200);
+    expect(genre).not.toBeNull();
+  });
+
+  it("should return genre if it is valid", async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post("/pasty/genres")
+      .set("x-auth-token", token)
+      .send({ name: "Romance" });
+
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("_id");
+    expect(res.body).toHaveProperty("name");
   });
 });
